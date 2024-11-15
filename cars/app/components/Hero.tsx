48025b80e76c6dfea814d543b028a40a1aa20495
sirMaxF <sirmaxfrei@mail.ru>
2024-11-15 14:47:55 +0300

    1214-1

--- cars/app/components/Hero.tsx
+++ cars/app/components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// https://vk.com/video-225998418_456239150?t=52m5s
+// https://vk.com/video-225998418_456239150?t=1h25m13s
 
 import Image from 'next/image';
 import CustomButton from './CustomButton';
